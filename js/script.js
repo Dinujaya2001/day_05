6e8fd4c2256ffee162b96eea9a5fabcf0677ddd7
Dinujaya2001 <dinujayatharaka2001@gmail.com>
2026-07-04 09:44:54 +0530

    Implement Customer class and instantiate objects for demonstration

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log("hello");
+class Customer{
+    name;
+    age;
+    address;
+    city;
+
+
+    constructor(name,age,address,city){
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.city = city;
+    }
+}
+
+let customer = new Customer("Tharaka",25,"No 12, Galle Road","Galle");
+console.log(customer);
+
+
+let customer2 = {
+    name: "Tharaka",
+    age: 25,
+    address: "No 12, Galle Road",
+    city: "Galle"
+};
+console.log(customer2);
